f1e390a33f9194574a6582a29f17d04af636e938
toxir0297-boop <toxir0297@gmail.com>
2026-05-26 12:47:53 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,7 @@
-export default function MyPlatforma() {
+export default function Home() {
   return (
     <div>
       <h1>myPlatforma 🚀 ishlayapti</h1>
     </div>
   );
-}
-export default function Home() {
-  return <h1>myPlatforma ishlayapti 🚀</h1>;
 }
